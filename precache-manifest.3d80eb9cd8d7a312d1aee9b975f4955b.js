9a13f48f83a5947bd55f028a9d39d3a0fce06f8f
petra2301 <hello@petragergely.dk>
2020-09-09 08:02:20 +0200

    Updates

---
+++ precache-manifest.3d80eb9cd8d7a312d1aee9b975f4955b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4926fa1c14e77378f2da745c60c0b55",
+    "url": "/north-reactapp/index.html"
+  },
+  {
+    "revision": "13ee40d1ca137d10f6d2",
+    "url": "/north-reactapp/static/css/main.8caeac86.chunk.css"
+  },
+  {
+    "revision": "4ec81a1037bb1b687e0c",
+    "url": "/north-reactapp/static/js/2.2e301283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/north-reactapp/static/js/2.2e301283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ee40d1ca137d10f6d2",
+    "url": "/north-reactapp/static/js/main.f4db3f7c.chunk.js"
+  },
+  {
+    "revision": "6ab2b2c3cf4b8a56af3b",
+    "url": "/north-reactapp/static/js/runtime-main.688ae8af.js"
+  },
+  {
+    "revision": "97decd2b78e0890e270894d96efe328f",
+    "url": "/north-reactapp/static/media/Inter-Bold.97decd2b.ttf"
+  },
+  {
+    "revision": "377afab3433cccf34a335842add702a0",
+    "url": "/north-reactapp/static/media/Inter-Light-BETA.377afab3.ttf"
+  },
+  {
+    "revision": "5f93153e545c9560d2afa542ebccbadb",
+    "url": "/north-reactapp/static/media/about.5f93153e.jpg"
+  },
+  {
+    "revision": "2012a5a23c533a241365710deab54782",
+    "url": "/north-reactapp/static/media/accomodation.2012a5a2.jpg"
+  },
+  {
+    "revision": "40d61b3e3ec403fd2057f87faeefc5e9",
+    "url": "/north-reactapp/static/media/app-banner.40d61b3e.png"
+  },
+  {
+    "revision": "79e5df86d6d2d1029906f140342ec4e1",
+    "url": "/north-reactapp/static/media/auroraborealis.79e5df86.jpg"
+  },
+  {
+    "revision": "9b95cd3aac875e2435cea682f2e07d50",
+    "url": "/north-reactapp/static/media/contact.9b95cd3a.jpg"
+  },
+  {
+    "revision": "9dab4c76587abbf437e5e8e091fe3026",
+    "url": "/north-reactapp/static/media/countries1.9dab4c76.jpg"
+  },
+  {
+    "revision": "9a06e625b72732c9d592ef3424ba14b8",
+    "url": "/north-reactapp/static/media/denmark.9a06e625.jpg"
+  },
+  {
+    "revision": "8b5b778cac519d9a7b1789be88d953d6",
+    "url": "/north-reactapp/static/media/faroeislands.8b5b778c.jpg"
+  },
+  {
+    "revision": "361694943d6d0de6084d689f95164d1f",
+    "url": "/north-reactapp/static/media/finland.36169494.jpg"
+  },
+  {
+    "revision": "fa2572f6cf6c61d04fae591719e5d9fa",
+    "url": "/north-reactapp/static/media/greenland.fa2572f6.jpg"
+  },
+  {
+    "revision": "e5e8472b648fef32a1361df064f8af8f",
+    "url": "/north-reactapp/static/media/homebanner.e5e8472b.jpg"
+  },
+  {
+    "revision": "5aced3b9db5e1781274cdde6a16cfb42",
+    "url": "/north-reactapp/static/media/iceland.5aced3b9.jpg"
+  },
+  {
+    "revision": "2632214f2515246a6b435b42a88eaa3b",
+    "url": "/north-reactapp/static/media/norway.2632214f.jpg"
+  },
+  {
+    "revision": "29c105508ea456c7ac9b4e66c095bbc9",
+    "url": "/north-reactapp/static/media/premadetrips.29c10550.jpg"
+  },
+  {
+    "revision": "80fe85425c95f60267e88258d52106d3",
+    "url": "/north-reactapp/static/media/scotland.80fe8542.jpg"
+  },
+  {
+    "revision": "fbb8a252cbdb920efed362063af0dbdb",
+    "url": "/north-reactapp/static/media/sweden.fbb8a252.jpg"
+  }
+]);
